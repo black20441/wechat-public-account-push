6bb5308c142064cb8f25e891d0be3859cbfad943
YaphetSZz <58318249+black20441@users.noreply.github.com>
2022-09-09 20:19:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "1f1b8ed2211a9e5a0f3c4771674affec",
+  APP_SECRET: "57940c9f26c1713e53a4b3b86f7e1989",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
