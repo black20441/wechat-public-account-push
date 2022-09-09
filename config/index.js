5a1f6439e099a9fb4f91310fec71450840a8fd80
YaphetSZz <58318249+black20441@users.noreply.github.com>
2022-09-09 19:20:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -69,9 +69,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "四川",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "成都",
 
   /** 重要节日相关 */
 
