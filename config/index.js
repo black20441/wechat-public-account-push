f280500d8049ced4ab9534b0ddcb85e74eee5b4b
YaphetSZz <58318249+black20441@users.noreply.github.com>
2022-09-09 20:11:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const config = {
       // 所在城市
       city: "肇庆",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: ""
     }, 
   ],
 
@@ -50,7 +50,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "89Un5ukv6W7qAw4UaV3vta1owGqDbvWhR3qJh0YoEzA",
+  CALLBACK_TEMPLATE_ID: "ftNh9g1dms3xVOX90Z9TLUuIKZ1FMndcyJHa4Vgkvvc",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
